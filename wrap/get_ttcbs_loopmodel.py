f11bb675b2d1e5db348bec64c3b154cdea5ff982
rjames-at-rwjames <rachel.james@eci.ox.ac.uk>
2017-02-13 09:53:14 +0200

    Add option "onlynew" to only run if the .synop file doesn't exist yet

--- wrap/get_ttcbs_loopmodel.py
+++ wrap/get_ttcbs_loopmodel.py
@@ -24,6 +24,7 @@ import cPickle
 import time as tmr
 import gc
 import sys,os
+import os.path
 cwd=os.getcwd()
 ### Add path to MetBot modules and import
 sys.path.append(cwd+'/..')
@@ -50,6 +51,7 @@ refsubset=True   # This is used if noaaolr=True to only look in time window
 hrwindow=49      # ... close (49 hours/ 2days) to flagged cloud band days
 synoptics=True   # Build tracks of cloud blobs that become TTT cloud bands
                  # ... which are then used to build TTT events.
+onlynew=True     # Option to only run if the synop file doesn't exist yet
 
 ### Ensure only look at Southern Africa
 sub="SA"
@@ -82,9 +84,9 @@ for d in range(ndset):
 
     for m in range(nmod):
         name=mnames[m]
-	mcnt=str(m)
+	    mcnt=str(m)
         print 'Running on ' + name
-	print 'This is model '+mcnt+' of '+nmstr+' in list'
+	    print 'This is model '+mcnt+' of '+nmstr+' in list'
 
         # Get details
         moddct=dsetdict.dset_deets[dset][name]
@@ -108,100 +110,109 @@ for d in range(ndset):
         my.mkdir_p(outdir)
         outsuf=outdir+name+'_'
 
-
-        ### Open OLR data
-        if olr0:
-            v=dset+"-olr-0-0"
-            daset, varstr, lev, drv = v.split('-')
-            ncout = mync.openolr_multi(infile,vname,name,\
-                                                        dataset=dset,subs=sub)
-            ndim = len(ncout)
-            if ndim==5:
-                olr,time,lat,lon,dtime = ncout
-            elif ndim==6:
-                olr, time, lat, lon, lev, dtime = ncout
-                olr=np.squeeze(olr)
+        # Check if file exists for this model
+        if onlynew:
+            chfile = outsuf + dset + '-OLR.synop'
+            if os.path.isfile(chfile):
+                print "MetBot already run on this model: "+name
+                continue # goes back to the beginning of the for loop
             else:
-                print 'Check number of levels in ncfile'
+                print "Running for the first time on: "+name
+        else:
 
-            ### Select data to run
-            ### Get time information
-            moddct = dsetdict.dset_deets[dset][name]
-            units = moddct['timeunit']
-            cal = moddct['calendar']
-            ### If testfile run on all days available
-            if testfile:
-                olr = olr[:, :, :];time = time[:];dtime = dtime[:]
-            else:
-                ### Find starting timestep
-                start = moddct['startdate']
-                ystart=int(start[0:4]);mstart=int(start[5:7]);dstart=int(start[8:10])
-                if cal=="360_day":
-                    startday=(ystart*360)+((mstart-1)*30)+dstart
-                    beginday=((int(beginatyr))*360)+1
-                    daysgap=beginday-startday+1
+            ### Open OLR data
+            if olr0:
+                v=dset+"-olr-0-0"
+                daset, varstr, lev, drv = v.split('-')
+                ncout = mync.openolr_multi(infile,vname,name,\
+                                                            dataset=dset,subs=sub)
+                ndim = len(ncout)
+                if ndim==5:
+                    olr,time,lat,lon,dtime = ncout
+                elif ndim==6:
+                    olr, time, lat, lon, lev, dtime = ncout
+                    olr=np.squeeze(olr)
                 else:
-                    startd=date(ystart,mstart,dstart)
-                    begind=date(int(beginatyr),01,01)
-                    daysgap=(begind-startd).days
-                olr=olr[daysgap:,:,:];time=time[daysgap:];dtime=dtime[daysgap:]
-            if testyear:
-                if cal=="360_day":
-                    olr, dtime, time = olr[:360, :, :], dtime[:360], time[:360]
+                    print 'Check number of levels in ncfile'
+
+                ### Select data to run
+                ### Get time information
+                moddct = dsetdict.dset_deets[dset][name]
+                units = moddct['timeunit']
+                cal = moddct['calendar']
+                ### If testfile run on all days available
+                if testfile:
+                    olr = olr[:, :, :];time = time[:];dtime = dtime[:]
                 else:
-                    olr, dtime, time = olr[:365,:,:],dtime[:365],time[:365]
+                    ### Find starting timestep
+                    start = moddct['startdate']
+                    ystart=int(start[0:4]);mstart=int(start[5:7]);dstart=int(start[8:10])
+                    if cal=="360_day":
+                        startday=(ystart*360)+((mstart-1)*30)+dstart
+                        beginday=((int(beginatyr))*360)+1
+                        daysgap=beginday-startday+1
+                    else:
+                        startd=date(ystart,mstart,dstart)
+                        begind=date(int(beginatyr),01,01)
+                        daysgap=(begind-startd).days
+                    olr=olr[daysgap:,:,:];time=time[daysgap:];dtime=dtime[daysgap:]
+                if testyear:
+                    if cal=="360_day":
+                        olr, dtime, time = olr[:360, :, :], dtime[:360], time[:360]
+                    else:
+                        olr, dtime, time = olr[:365,:,:],dtime[:365],time[:365]
 
-            ### Plot olr dist to check threshold
-            if getdistr: showme = blb.gethists(olr,time,lat,lon,v,sub=sub,figd=outsuf)
-            plt.ion()
+                ### Plot olr dist to check threshold
+                if getdistr: showme = blb.gethists(olr,time,lat,lon,v,sub=sub,figd=outsuf)
+                plt.ion()
 
-            ### Get mbs 0-0
-            if getmbs:
-                mbs, mbt, chull = blb.MetBlobs(olr,dtime,time,lat,lon,v,\
-                                               sub=sub,showblobs=showblb,interact=intract)
-                blb.mbsave(outsuf+v+".mbs",mbs,mbt,chull)
-                del mbs,mbt,chull
-
-                ### Get mbs 0-all
-                if olrall:
-                    refmbsstr=dset+"-olr-0-0"
-                    refmbs,refmbt,refch = blb.mbopen(outsuf+refmbsstr+".mbs")
-                    reftime=refmbs[:,0]
-                    v=dset+"-olr-0-all"
-                    daset,varstr, lev, drv = v.split('-')
-                    exec("ixt,[time,%s,dtime]=\
-                          my.ixtwindow(reftime,time,hrwindow,time,%s,dtime)"\
-                           %(varstr,varstr) )
+                ### Get mbs 0-0
+                if getmbs:
                     mbs, mbt, chull = blb.MetBlobs(olr,dtime,time,lat,lon,v,\
-                                              sub=sub,showblobs=showblb,interact=False)
+                                                   sub=sub,showblobs=showblb,interact=intract)
                     blb.mbsave(outsuf+v+".mbs",mbs,mbt,chull)
                     del mbs,mbt,chull
 
-                ### Get mbs 0-full
-                if olrfull:
-                    v=dset+"-olr-0-full"
-                    daset,varstr, lev, drv = v.split('-')
-                    mbs, mbt, chull = blb.MetBlobs(olr,dtime,time,lat,lon,v,\
-                                              sub=sub,showblobs=showblb,interact=False)
-                    blb.mbsave(outsuf+v+".mbs",mbs,mbt,chull)
-                    del mbs,mbt,chull
+                    ### Get mbs 0-all
+                    if olrall:
+                        refmbsstr=dset+"-olr-0-0"
+                        refmbs,refmbt,refch = blb.mbopen(outsuf+refmbsstr+".mbs")
+                        reftime=refmbs[:,0]
+                        v=dset+"-olr-0-all"
+                        daset,varstr, lev, drv = v.split('-')
+                        exec("ixt,[time,%s,dtime]=\
+                              my.ixtwindow(reftime,time,hrwindow,time,%s,dtime)"\
+                               %(varstr,varstr) )
+                        mbs, mbt, chull = blb.MetBlobs(olr,dtime,time,lat,lon,v,\
+                                                  sub=sub,showblobs=showblb,interact=False)
+                        blb.mbsave(outsuf+v+".mbs",mbs,mbt,chull)
+                        del mbs,mbt,chull
 
-        ### Get synop file
-        if synoptics:
-            refmbstr=dset+"-olr-0-0"
-            refall=dset+"-olr-0-all"
-            reffull=dset+"-olr-0-full"
-            metblobslist=[refmbstr,refall,reffull]
-            mfilelist=[outsuf+j+'.mbs' for j in metblobslist]
-            print mfilelist
-            s = sy.SynopticEvents(metblobslist,mfilelist,hrwindow=hrwindow)
-            s.buildtracks()
-            s.buildevents(basetrkkey=refmbsstr)
-            u = s.uniqueevents()
-            s.save(outsuf+dset+'-OLR.synop')
-            del s
+                    ### Get mbs 0-full
+                    if olrfull:
+                        v=dset+"-olr-0-full"
+                        daset,varstr, lev, drv = v.split('-')
+                        mbs, mbt, chull = blb.MetBlobs(olr,dtime,time,lat,lon,v,\
+                                                  sub=sub,showblobs=showblb,interact=False)
+                        blb.mbsave(outsuf+v+".mbs",mbs,mbt,chull)
+                        del mbs,mbt,chull
 
-	print 'Finished running on ' + name
+            ### Get synop file
+            if synoptics:
+                refmbstr=dset+"-olr-0-0"
+                refall=dset+"-olr-0-all"
+                reffull=dset+"-olr-0-full"
+                metblobslist=[refmbstr,refall,reffull]
+                mfilelist=[outsuf+j+'.mbs' for j in metblobslist]
+                print mfilelist
+                s = sy.SynopticEvents(metblobslist,mfilelist,hrwindow=hrwindow)
+                s.buildtracks()
+                s.buildevents(basetrkkey=refmbsstr)
+                u = s.uniqueevents()
+                s.save(outsuf+dset+'-OLR.synop')
+                del s
+
+        print 'Finished running on ' + name
         print 'This is model '+mcnt+' of '+nmstr+' in list'
 
 print 'TOTAL TIME TAKEN FOR get_ttcbs_loopmodel.py is:',(tmr.time()-tstart)/60,'mins'
