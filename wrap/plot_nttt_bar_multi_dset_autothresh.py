57c5e46796fea23984c43b27e9410a9eac685354
rjames-at-rwjames <rachel.james@eci.ox.ac.uk>
2017-03-13 12:17:38 +0200

    New wrapper with scatterplot of nttt vs. thresh

--- wrap/plot_nttt_bar_multi_dset_autothresh.py
+++ wrap/plot_nttt_bar_multi_dset_autothresh.py
@@ -96,6 +96,7 @@ for t in range(nthresh):
                         thresh = line.split()[2]
                         print 'thresh='+str(thresh)
 
+            thresh = int(thresh)
 
             if thnames[t]=='actual':
                 thisthresh=thresh
