676def7089f91406207063c9d3532ed20f64b3c0
rjames-at-rwjames <rachel.james@eci.ox.ac.uk>
2017-09-29 16:59:13 +0200

    Edits to precip and olr wrappers to plot composite anomaly

--- MetBot/AdvancedPlots.py
+++ MetBot/AdvancedPlots.py
@@ -396,7 +396,7 @@ def gridrainmap_season(s,eventkeys,rain,rlat,rlon,rdtime,cl,season='coreseason',
             cticks = clevs
             cm = plt.cm.YlGnBu
         elif ptype=='comp_anom_ttt':
-            clevs = np.arange(-4, 4, 0.5)
+            clevs = np.arange(-4, 4.5, 0.5)
             cticks = clevs
             cm = plt.cm.seismic_r
 
