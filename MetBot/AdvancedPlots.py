6926e17174ed079b7ad36b8766572b19f6cbf0d0
rjames-at-rwjames <rachel.james@eci.ox.ac.uk>
2017-10-09 12:03:17 +0200

    New wrapper to plot metbot composites of omega

--- MetBot/AdvancedPlots.py
+++ MetBot/AdvancedPlots.py
@@ -1208,12 +1208,12 @@ def gridvarmap_season(s,eventkeys,varstr,vardata,varlat,varlon,vardtime,olrunits
 
         if ptype=='comp_anom_ag':
             if varstr=='omega':
-                clevs = np.arange(-0.15, 0.18, 0.03)
+                clevs = np.arange(-0.06, 0.07, 0.01)
                 cm = plt.cm.seismic
         elif ptype == 'comp_anom_cnt':
             if varstr == 'omega':
                 clevs= np.arange(30,75,5)
-                cm = plt.cm.seismic
+                cm = plt.cm.seismic_r
 
         if test:
             cs = m.contourf(plon, plat, data4plot, cmap=cm, extend='both')
